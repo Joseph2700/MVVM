eb4728e19969cdb3b06c3706fe67b8b5a539d157
Joseph2700 <50160800+Joseph2700@users.noreply.github.com>
2020-02-13 21:02:18 -0600

    .

--- Tarea1/Tarea1/Tarea1.Android/Resources/Resource.designer.cs
+++ Tarea1/Tarea1/Tarea1.Android/Resources/Resource.designer.cs
@@ -6788,14 +6788,38 @@ namespace Tarea1.Droid
 		public partial class Mipmap
 		{
 			
+			// aapt resource value: 0x7F0C0008
+			public const int icon = 2131492872;
+			
+			// aapt resource value: 0x7F0C0009
+			public const int icon_round = 2131492873;
+			
 			// aapt resource value: 0x7F0C0000
-			public const int icon = 2131492864;
+			public const int ic_bookmark = 2131492864;
 			
 			// aapt resource value: 0x7F0C0001
-			public const int icon_round = 2131492865;
+			public const int ic_camera = 2131492865;
 			
 			// aapt resource value: 0x7F0C0002
-			public const int launcher_foreground = 2131492866;
+			public const int ic_checkmark = 2131492866;
+			
+			// aapt resource value: 0x7F0C0003
+			public const int ic_eye = 2131492867;
+			
+			// aapt resource value: 0x7F0C0004
+			public const int ic_home = 2131492868;
+			
+			// aapt resource value: 0x7F0C0005
+			public const int ic_location = 2131492869;
+			
+			// aapt resource value: 0x7F0C0006
+			public const int ic_translate = 2131492870;
+			
+			// aapt resource value: 0x7F0C0007
+			public const int ic_user = 2131492871;
+			
+			// aapt resource value: 0x7F0C000A
+			public const int launcher_foreground = 2131492874;
 			
 			static Mipmap()
 			{
