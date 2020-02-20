fbdbb48f611e96a3c0adf7cbc1bc15b4f082332e
Joseph2700 <50160800+Joseph2700@users.noreply.github.com>
2020-02-19 23:43:23 -0600

    Added Folders
    Added the Models, TabbedPages, View Models, and Views Folder

---
+++ Tarea1/Tarea1/Tarea1/ViewModels/SignUpPageViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Tarea1.ViewModels
+{
+    class SignUpPageViewModel : INotifyPropertyChanged
+    {
+        public SignUpPageViewModel()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
