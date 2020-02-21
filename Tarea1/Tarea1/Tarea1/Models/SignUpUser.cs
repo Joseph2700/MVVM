8998aeb9695878b51b765f09f042a04182f94b01
Joseph2700 <50160800+Joseph2700@users.noreply.github.com>
2020-02-20 18:54:08 -0600

    Binding SignUP
    Completed the binding for the sign up

--- Tarea1/Tarea1/Tarea1/Models/SignUpUser.cs
+++ Tarea1/Tarea1/Tarea1/Models/SignUpUser.cs
@@ -10,5 +10,6 @@ namespace Tarea1.Models
         public string UserName { get; set; }
         public string Password { get; set; }
         public string RepeatPassword { get; set; }
+        public bool IsPassword { get; set; } = true;
     }
 }
